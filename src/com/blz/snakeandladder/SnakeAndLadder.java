76218f92d34522cab1fe62843b651bbdadef5ad1
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 13:04:26 +0530

    feat:Switch case,no paly,Ladder,Snake

--- src/com/blz/snakeandladder/SnakeAndLadder.java
+++ src/com/blz/snakeandladder/SnakeAndLadder.java
@@ -1,9 +1,38 @@
 package com.blz.snakeandladder;
 
 public class SnakeAndLadder {
+	public int rollDice() {
+		int randValue = (int) (Math.random() * 10) % 6 + 1;
+		return randValue;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int randValue=(int) (Math.random()*10)%6+1;
-		System.out.println(randValue);
+		SnakeAndLadder ch = new SnakeAndLadder();
+		int currentPosition = 0;
+		do {
+			int randOption = (int) (Math.random() * 10) % 3 + 1;
+			int rollDice;
+			System.out.println(randOption);
+			switch (randOption) {
+			case 1:
+				System.out.println("No Play");
+				break;
+			case 2:
+				System.out.println("In Ladder");
+				rollDice = ch.rollDice();
+				currentPosition += rollDice;
+				break;
+			case 3:
+				System.out.println("In Snake");
+				rollDice = ch.rollDice();
+				currentPosition -= rollDice;
+				break;
+			default:
+				System.out.println("Erro In randOption");
+			}
+			System.out.println("Current Position : " + currentPosition);
+
+		} while (currentPosition <= 50);
+
 	}
 }
