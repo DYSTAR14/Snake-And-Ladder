1b91263b941417776fa6b973038ee95d8e1a60f6
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 13:32:12 +0530

    feat:Two Player Play the game

--- src/com/blz/snakeandladder/SnakeAndLadder.java
+++ src/com/blz/snakeandladder/SnakeAndLadder.java
@@ -2,48 +2,64 @@ package com.blz.snakeandladder;
 
 public class SnakeAndLadder {
 	static final int WINNINGPOSITION = 100;
+	static int countDice = 0;
+	static SnakeAndLadder ch = new SnakeAndLadder();
 
 	public int rollDice() {
 		int randValue = (int) (Math.random() * 10) % 6 + 1;
 		return randValue;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SnakeAndLadder ch = new SnakeAndLadder();
-		int currentPosition = 0, countDice = 0;
-		do {
-			int randOption = (int) (Math.random() * 10) % 3 + 1;
-			int rollDice;
-			switch (randOption) {
-			case 1:
-				System.out.println("No Play");
-				break;
-			case 2:
-				System.out.println("In Ladder");
+	public int randOptionCase(int randOption,int currentPosition) {
+		int rollDice;
+		switch (randOption) {
+		case 1:
+			System.out.println("No Play");
+			break;
+		case 2:
+			System.out.println("In Ladder");
+			for (int i = 0; i < 2; i++) {
 				rollDice = ch.rollDice();
 				countDice += 1;
 				currentPosition += rollDice;
 				if (currentPosition > 100) {
 					currentPosition -= rollDice;
 				}
-				break;
-			case 3:
-				System.out.println("In Snake");
-				rollDice = ch.rollDice();
-				countDice += 1;
-				currentPosition -= rollDice;
-				if (currentPosition < 0) {
-					currentPosition = 0;
-				}
-				break;
-			default:
-				System.out.println("Erro In randOption");
 			}
+			break;
+		case 3:
+			System.out.println("In Snake");
+			rollDice = ch.rollDice();
+			countDice += 1;
+			currentPosition -= rollDice;
+			if (currentPosition < 0) {
+				currentPosition = 0;
+			}
+			break;
+		default:
+			System.out.println("Erro In randOption");
+		}
+		return currentPosition;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int randOption;
+		int currentPositionPlayer1 = 0,currentPositionPlayer2 = 0;
+		do {
+			randOption = (int) (Math.random() * 10) % 3 + 1;
+			currentPositionPlayer1=ch.randOptionCase(randOption, currentPositionPlayer1);
+			System.out.println("Current Position Player 1 : " + currentPositionPlayer1);
+			
+			randOption = (int) (Math.random() * 10) % 3 + 1;
+			currentPositionPlayer2=ch.randOptionCase(randOption, currentPositionPlayer2);
+			System.out.println("Current Position Player 2 : " + currentPositionPlayer2);
 
-			System.out.println("Current Position : " + currentPosition);
-
-		} while (currentPosition < 100);
+		} while (currentPositionPlayer1!=WINNINGPOSITION && currentPositionPlayer2!=WINNINGPOSITION);
+		if (currentPositionPlayer1==100) {
+			System.out.println("Player one is won :"+currentPositionPlayer1);
+		}
+		else if (currentPositionPlayer2==100) {
+			System.out.println("Player Secon is won :"+currentPositionPlayer2);
+		}
 		System.out.println("Total Number of time RollDice : " + countDice);
 	}
 }
