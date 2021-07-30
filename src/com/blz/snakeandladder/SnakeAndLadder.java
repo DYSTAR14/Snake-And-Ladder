75d3704f2e6261ada63dd6d71c4bbc6e3979c9d7
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 12:54:45 +0530

    feat:Rolls the dice 1 to 6

--- src/com/blz/snakeandladder/SnakeAndLadder.java
+++ src/com/blz/snakeandladder/SnakeAndLadder.java
@@ -3,8 +3,7 @@ package com.blz.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name="Deepak";
-		int position=0;
-		System.out.println(name+" Current Position : "+position);
+		int randValue=(int) (Math.random()*10)%6+1;
+		System.out.println(randValue);
 	}
 }
